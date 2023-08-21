810723308dcfe9e3541947f9163d36c0cf70a44f
Dan Web <128343993+therealdanweb@users.noreply.github.com>
2023-08-21 11:43:30 +0300

    Update suma37.c

--- suma37.c
+++ suma37.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main()
+{
+	char c; unsigned s = 0;
+
+	while(scanf("%c", &c) != EOF && c >= '0' && c <= '9') {
+		if (c >= '3' && c <= '7') s += (c - '0');
+	}
+
+	printf("%u", s);
+
+	return 0;
+}
